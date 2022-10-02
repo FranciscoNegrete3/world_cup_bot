1bd59653d16dab79658e7c6795b7dca1452d064f
FranciscoNegrete3 <fran.negrete@hotmail.com>
2022-10-02 14:05:39 -0300

    Commmit

--- main.py
+++ main.py
@@ -5,16 +5,21 @@ import tweepy
 from credentials import access_token, access_token_secret, api_key, api_key_secret
 
 client = tweepy.Client(
-    consumer_key= api_key,
-    consumer_secret= api_key_secret,
-    access_token= access_token ,
-    access_token_secret=access_token_secret
+    consumer_key=api_key,
+    consumer_secret=api_key_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret,
 )
 
-print(client)
 
-response = client.create_tweet(text="Hola Copa Qatar!")
+def create_tweet(string: str):
+
+    response = client.create_tweet(text=string)
+
+    return response
 
 
-response_dos = 
-print(response)
+print(create_tweet(input("Enter a Tweet you want to post: ")))
+
+
+print("Hola")
